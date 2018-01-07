b19ca2625b319928bed42a7a9e4b00e52a1e7e6e
zacks <bluemilk1995@gmail.com>
2018-01-08 04:31:19 +0800

    新增人員資料

---
+++ src/components/Staff/Fulltime.vue
@@ -0,0 +1,117 @@
+<template>
+<div id="member">
+  <div class="switch">
+    <div class="link"><router-link to='/Staff/Fulltime'>專任師資</router-link></div>
+    <div class="link"><router-link to='/Staff/Parttime'>兼任師資</router-link></div>
+    <div class="link"><router-link to='/Staff/Director'>歷屆系主任</router-link></div>
+    <div class="link"><router-link to='/Staff/Administrative'>行政人員</router-link></div>
+  </div>
+  <div class="location-contain">
+    <div class="locations" v-for="(location, index) in locations" :key="index">
+      <div class="place">
+        <div class="pic">
+          <img :src="location.img" height="150"/>
+        </div>
+        <div class="text">
+          <h2>{{ location.name }}</h2>
+          <p>{{ location.desc }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      locations: [
+        {
+          name: '劉皇杏',
+          img: 'http://www.tfux.tku.edu.tw/files/users/pic/cache.25_7dc7fdc7.jpg.w95_h117.jpg',
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+        {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+#member
+  display: flex
+  flex-direction: column
+  margin: 10px
+  .switch
+    display: flex
+    flex-direction: row
+    justify-content: center
+    margin: 30px 0
+    .link
+      display: inline-block
+      a
+        text-decoration: none
+        color: #333
+        padding: 10px 20px
+        background: #ccc
+        transition: color 0.1s, background-color 0.1s
+        letter-spacing: 1px
+        font-weight: 900
+        position: relative
+        &:hover, &:focus, &:active
+          background: #eee
+          // color: #999
+        &::before
+          content: ''
+          display: block
+          position: absolute
+          bottom: 0px
+          left: 0
+          height: 3px
+          width: 100%
+          background-color: #000
+          transform-origin: right top
+          transform: scale(0, 1)
+          transition: color 0.1s,transform 0.2s ease-out
+        &:active::before
+          background-color: #000
+        &:hover::before, &:focus::before
+          transform-origin: left top
+          transform: scale(1, 1)
+  .location-contain
+    display: flex
+    justify-content: space-around
+    flex-wrap: wrap
+    .locations
+      height: 100%
+      .place
+        display: flex
+        flex-direction: column
+        width: 250px
+        height: 100%
+        background: white
+        border: 1px solid #ddd
+        padding: 20px 20px
+        .pic
+          align-self: center
+        .text
+
+
+
+</style>
