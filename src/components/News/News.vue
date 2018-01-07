a1f0eb5718f328d52763934b08cb63c1d1ef230f
zacks <bluemilk1995@gmail.com>
2018-01-08 03:28:49 +0800

    小修改

---
+++ src/components/News/News.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+#news
+  Slider
+  router-view
+</template>
+<script>
+import Slider from './Slider'
+// import Content from './Content'
+
+export default {
+  components: {
+    Slider
+  }
+}
+</script>
+<style lang="sass">
+#news
+  display: flex
+  flex-direction: column
+</style>
