cf0312f1b994c7adea4d927c759637530d1f03bd
tommy1994111 <tommy1994111@gmail.com>
2018-01-07 23:58:36 +0800

    系所成員 新增 切換分類menu

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,4 +1,11 @@
 <template>
+<div id="member">
+  <div class="switch">
+    <div class="link"><a href="#">專任師資</a></div>
+    <div class="link"><a href="#">兼任師資</a></div>
+    <div class="link"><a href="#">歷屆系主任</a></div>
+    <div class="link"><a href="#">行政人員</a></div>
+  </div>
   <div class="location-contain">
     <div class="locations" v-for="(location, index) in locations" :key="index">
       <div class="place">
@@ -12,6 +19,7 @@
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -46,24 +54,64 @@ export default {
 </script>
 
 <style lang="sass">
-.location-contain
+#member
   display: flex
-  justify-content: space-around
-  flex-wrap: wrap
+  flex-direction: column
   margin: 10px
-  .locations
-    height: 100%
-    .place
-      display: flex
-      flex-direction: column
-      width: 250px
+  .switch
+    display: flex
+    flex-direction: row
+    justify-content: center
+    margin: 30px 0
+    .link
+      display: inline-block
+      a
+        text-decoration: none
+        color: #333
+        padding: 10px 20px
+        background: #ccc
+        transition: color 0.1s, background-color 0.1s
+        letter-spacing: 1px
+        font-weight: 900
+        position: relative
+        &:hover, &:focus, &:active
+          background: #eee
+          // color: #999
+        &::before
+          content: ''
+          display: block
+          position: absolute
+          bottom: 0px
+          left: 0
+          height: 3px
+          width: 100%
+          background-color: #000
+          transform-origin: right top
+          transform: scale(0, 1)
+          transition: color 0.1s,transform 0.2s ease-out
+        &:active::before
+          background-color: #000
+        &:hover::before, &:focus::before
+          transform-origin: left top
+          transform: scale(1, 1)
+  .location-contain
+    display: flex
+    justify-content: space-around
+    flex-wrap: wrap
+    .locations
       height: 100%
-      background: white
-      border: 1px solid #ddd
-      padding: 20px 20px
-      .pic
-        align-self: center
-      .text
+      .place
+        display: flex
+        flex-direction: column
+        width: 250px
+        height: 100%
+        background: white
+        border: 1px solid #ddd
+        padding: 20px 20px
+        .pic
+          align-self: center
+        .text
+
 
 
 </style>
