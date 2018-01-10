be0faea3dd09886196c431a3afd35e04ec4fdd8c
zacks <bluemilk1995@gmail.com>
2018-01-11 03:25:54 +0800

    照片牆

--- src/components/News/SubMenu/StudentInformation.vue
+++ src/components/News/SubMenu/StudentInformation.vue
@@ -1,15 +1,30 @@
 <template>
-<div id="StudentInformation">StudentInformation</div>
+  <div id="studentInformation">
+    <!-- directive -->
+    <div class="images" v-viewer>
+      <img src="https://p2.bahamut.com.tw/S/2KU/53/0001480453.JPG">
+      <img src="https://p2.bahamut.com.tw/S/2KU/17/fea2935ecc86354cfdcc6b983c7zj6h4.JPG">
+      ...
+    </div>
+    <!-- component -->
+    <viewer :images="images">
+      <img v-for="src in images" :src="src" :key="src">
+    </viewer>
+  </div>
 </template>
 <script>
-import {mapState} from 'vuex'
-
-export default {
-  
-}
+  import Viewer from 'v-viewer'
+  import Vue from 'vue'
+  Vue.use(Viewer)
+  export default {
+    data() {
+      images: ['1.jpg', '2.jpg']
+    }
+  }
 </script>
+
 <style lang="sass">
-#StudentInformation
+#studentInformation
   border: 1px solid #ccc
   border-radius: 5px
   padding: 20px
