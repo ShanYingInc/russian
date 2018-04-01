767c011a90d85f3229f538cc9ca86d720b28d7e6
tommy1994111 <tommy1994111@gmail.com>
2018-04-01 21:08:50 +0800

    編輯actions.js initData平行執行化

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,39 +4,49 @@ import Api from '../api'
 const api = new Api()
 
 export const actions = {
+  async updateMemberData ({ commit }) {
+    const response = await api.member.get()
+    commit(types.UPDATE_STAFF_DATA, await response.data.data)
+  },
+  async updateNewsData ({ commit }) {
+    const response = await api.news.get()
+    commit(types.UPDATE_NEWS_DATA, await response.data.data.reverse())
+  },
+  async updateVideoData ({ commit }) {
+    const response = await api.video.get()
+    commit(types.UPDATE_VIDEO_DATA, await response.data.data)
+  },
+  async updateNavigationData ({ commit }) {
+    const response = await api.navigation.get()
+    commit(types.UPDATE_NAVIGATION_DATA, await response.data.data)
+  },
+  async updateContentData ({ commit }) {
+    const response = await api.content.get()
+    commit(types.UPDATE_CONTENT_DATA, await response.data)
+  },
+  async updateAlbumData ({ commit }) {
+    const response = await api.album.get()
+    commit(types.UPDATE_ALBUM_DATA, await response.data.data)
+  },
+  async updateBannerData ({ commit }) {
+    const response = await api.banner.get()
+    commit(types.UPDATE_BANNER_DATA, await response.data)
+  },
+  async updateTeachingExcellenceData ({ commit }) {
+    const response = await api.teachingExcellence.get()
+    commit(types.UPDATE_TEACHING_EXCELLENCE_DATA, await response.data.data)
+  },
   async initData ({ commit, state, dispatch }) {
-
-    api.member.get().then(async response => {
-      commit(types.UPDATE_STAFF_DATA, await response.data.data)
-    })
-
-    api.news.get().then(async response => {
-      commit(types.UPDATE_NEWS_DATA, await response.data.data.reverse())
-    })
-
-    api.video.get().then(async response => {
-      commit(types.UPDATE_VIDEO_DATA, await response.data.data)
-    })
-
-    api.navigation.get().then(async response => {
-      commit(types.UPDATE_NAVIGATION_DATA, await response.data.data)
-    })
-
-    api.content.get().then(async response => {
-      commit(types.UPDATE_CONTENT_DATA, await response.data)
-    })
-
-    api.album.get().then(async response => {
-      commit(types.UPDATE_ALBUM_DATA, await response.data.data)
-    })
-
-    api.banner.get().then(async response => {
-      commit(types.UPDATE_BANNER_DATA, await response.data)
-    })
-
-    api.teachingExcellence.get().then(async response => {
-      commit(types.UPDATE_TEACHING_EXCELLENCE_DATA, await response.data.data)
-    })
+    Promise.all([
+      dispatch('updateMemberData'),
+      dispatch('updateNewsData'),
+      dispatch('updateVideoData'),
+      dispatch('updateNavigationData'),
+      dispatch('updateContentData'),
+      dispatch('updateAlbumData'),
+      dispatch('updateBannerData'),
+      dispatch('updateTeachingExcellenceData')
+    ])
   },
   async switchLanguage ({ commit, state, dispatch }, language) {
     api.switchLanguage(await language)
