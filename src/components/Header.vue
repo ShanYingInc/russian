52b08f060b53ad6186c9a00f056004cce1b77e0b
tommy1994111 <tommy1994111@gmail.com>
2018-05-23 07:18:37 +0800

    更換導覽列為原生bootstrap4 Navbar(原本為使用bootstrap-vue)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,49 +15,62 @@
           span(@click="switchLanguage(`en`)")  English |
         li
           span(@click="switchLanguage(`ru`)")  Русский
-  .nav
-    b-nav
-      b-nav-item(to="/newsList") 最新消息
-      b-nav-item-dropdown#nav-2(text='系所簡介')
-        b-dropdown-item(to="/Introduction/Important") 重要記事
-        b-dropdown-item(to="/Introduction/Vision") 願景及策略
-      b-nav-item-dropdown#nav-3(text='師資陣容')
-        b-dropdown-item(to="/Staff/Fulltime") 專任師資
-        b-dropdown-item(to="/Staff/Parttime") 兼任師資
-        b-dropdown-item(to="/Staff/Director") 歷屆系主任
-        b-dropdown-item(to="/Staff/Administrative") 行政人員
-        b-dropdown-item(to="/Staff/Tutor") 各班導師
-        //- b-dropdown-item(to="/Enrollment/Answer") 招生訊息
-      //- b-nav-item-dropdown#nav-4(text='學期課表')
-      //-   b-dropdown-item(to="/Class/Courseinformation") 課程資訊
-      //-   b-dropdown-item(to="/Class/Course") 課程架構
-      //-   b-dropdown-item(to="/Class/Alternative") 必選修科目
-      //-   b-dropdown-item(to="/Class/Required") 替代科目
-      //-   b-dropdown-item(to="/Class/Relevant") 相關修業規定
-      //-   b-dropdown-item(to="/Class/Credit") 學分學程
-      //-   b-dropdown-item(to="/Class/Auxiliary") 輔系雙學位
-      //- b-nav-item-dropdown#nav-5(text='招生資訊')
-      //-   b-dropdown-item(to="/Enrollment/Admission") 申請入學
-      //-   b-dropdown-item(to="/Enrollment/Answer") 相關問題和解答
-      //-   b-dropdown-item(to="/Winning") 獲獎紀錄
-      //- b-nav-item-dropdown#nav-6(text='教卓計畫')
-      //-   b-dropdown-item(to="/Teaching/Teaching100") 100學年度
-      //-   b-dropdown-item(to="/Teaching/Teaching101") 101學年度
-      //-   b-dropdown-item(to="/Teaching/Teaching102") 102學年度
-      //- b-nav-item(to="/Friends/Friendsintroduction") 系友專區
-      b-nav-item-dropdown(v-for="(nav, index) in navigations", :key="index", :text='nav.name', :id="`nav_` + nav.navigation_id")
-        b-dropdown-item(v-for="(subNav, subIndex) in nav.subItems", :key="subIndex", :to="`/content?parent=`+nav.name+`&contentKey=`+subNav.content_key") {{ subNav.name }}
-      b-nav-item-dropdown#nav-4(text='教卓計畫')
-        b-dropdown-item(v-for="year in teachingExcellenceYears", :key="year", :to="`/TeachingExcellenceList/` + year") {{ year }}學年度
-      b-nav-item-dropdown#nav-4(text='活動花絮')
-        b-dropdown-item(to="/eventHighlignt") 活動花絮
-        b-dropdown-item(to="/Performance") 戲劇公演
-
+  //- .nav
+  //-   b-nav
+  //-     b-nav-item(to="/newsList") 最新消息
+  //-     b-nav-item-dropdown#nav-2(text='系所簡介')
+  //-       b-dropdown-item(to="/Introduction/Important") 重要記事
+  //-       b-dropdown-item(to="/Introduction/Vision") 願景及策略
+  //-     b-nav-item-dropdown#nav-3(text='師資陣容')
+  //-       b-dropdown-item(to="/Staff/Fulltime") 專任師資
+  //-       b-dropdown-item(to="/Staff/Parttime") 兼任師資
+  //-       b-dropdown-item(to="/Staff/Director") 歷屆系主任
+  //-       b-dropdown-item(to="/Staff/Administrative") 行政人員
+  //-       b-dropdown-item(to="/Staff/Tutor") 各班導師
+  //-       //- b-dropdown-item(to="/Enrollment/Answer") 招生訊息
+  //-     //- b-nav-item-dropdown#nav-4(text='學期課表')
+  //-     //-   b-dropdown-item(to="/Class/Courseinformation") 課程資訊
+  //-     //-   b-dropdown-item(to="/Class/Course") 課程架構
+  //-     //-   b-dropdown-item(to="/Class/Alternative") 必選修科目
+  //-     //-   b-dropdown-item(to="/Class/Required") 替代科目
+  //-     //-   b-dropdown-item(to="/Class/Relevant") 相關修業規定
+  //-     //-   b-dropdown-item(to="/Class/Credit") 學分學程
+  //-     //-   b-dropdown-item(to="/Class/Auxiliary") 輔系雙學位
+  //-     //- b-nav-item-dropdown#nav-5(text='招生資訊')
+  //-     //-   b-dropdown-item(to="/Enrollment/Admission") 申請入學
+  //-     //-   b-dropdown-item(to="/Enrollment/Answer") 相關問題和解答
+  //-     //-   b-dropdown-item(to="/Winning") 獲獎紀錄
+  //-     //- b-nav-item-dropdown#nav-6(text='教卓計畫')
+  //-     //-   b-dropdown-item(to="/Teaching/Teaching100") 100學年度
+  //-     //-   b-dropdown-item(to="/Teaching/Teaching101") 101學年度
+  //-     //-   b-dropdown-item(to="/Teaching/Teaching102") 102學年度
+  //-     //- b-nav-item(to="/Friends/Friendsintroduction") 系友專區
+  //-     b-nav-item-dropdown(
+  //-       v-for="(nav, index) in navigations",
+  //-       :key="index",
+  //-       :text='nav.name',
+  //-       :id="`nav_` + nav.navigation_id"
+  //-     )
+  //-       b-dropdown-item(
+  //-         v-for="(subNav, subIndex) in nav.subItems",
+  //-         :key="subIndex",
+  //-         :to="`/content?parent=`+nav.name+`&contentKey=`+subNav.content_key"
+  //-       ) {{ subNav.name }}
+  //-     b-nav-item-dropdown#nav-4(text='教卓計畫')
+  //-       b-dropdown-item(v-for="year in teachingExcellenceYears", :key="year", :to="`/TeachingExcellenceList/` + year") {{ year }}學年度
+  //-     b-nav-item-dropdown#nav-4(text='活動花絮')
+  //-       b-dropdown-item(to="/eventHighlignt") 活動花絮
+  //-       b-dropdown-item(to="/Performance") 戲劇公演
+  NavBar
 </template>
 <script>
+import NavBar from './Navbar'
 import { mapState, mapActions, mapGetters } from 'vuex'
 
 export default {
+  components: {
+    NavBar
+  },
   computed: {
     ...mapState({
       navigations: 'navigation'
@@ -81,22 +94,22 @@ export default {
     padding: 10px 0 0 0
   .header
     padding: 10px
-  .nav
-    font-family: "Microsoft YaHei"
-    width: 100%
-    height: 100%
-    background: #005792
-    .dropdown-menu > li > a
-      color: white
-    .dropdown-item.active
-      background: white
-    ul
-      li
-        a
-          color: white
-        div
-          a
-            color: black
+  // .nav
+  //   font-family: "Microsoft YaHei"
+  //   width: 100%
+  //   height: 100%
+  //   background: #005792
+  //   .dropdown-menu > li > a
+  //     color: white
+  //   .dropdown-item.active
+  //     background: white
+  //   ul
+  //     li
+  //       a
+  //         color: white
+  //       div
+  //         a
+  //           color: black
 
 .goindex ul li
   display: inline;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+  nav#navbar.navbar.navbar-expand-lg.navbar-dark
+    button.navbar-toggler(
+      type='button',
+      data-toggle='collapse',
+      data-target='#navbarNav',
+      aria-controls='navbarNav',
+      aria-expanded='false',
+      aria-label='Toggle navigation'
+    )
+      span.navbar-toggler-icon
+    #navbarNav.collapse.navbar-collapse
+      ul.navbar-nav
+        li.nav-item
+          router-link.nav-link.active(to="/newsList") 最新消息
+        li.nav-item.dropdown
+          a#nav1.nav-link.active.dropdown-toggle(
+            href='#',
+            role='button',
+            data-toggle='dropdown',
+            aria-haspopup='true',
+            aria-expanded='false'
+          ) 系所簡介
+          .dropdown-menu(aria-labelledby='nav1')
+            router-link.dropdown-item(to="/Introduction/Important") 重要記事
+            router-link.dropdown-item(to="/Introduction/Vision") 願景及策略
+        li.nav-item.dropdown
+          a#nav2.nav-link.active.dropdown-toggle(
+            href='#',
+            role='button',
+            data-toggle='dropdown',
+            aria-haspopup='true',
+            aria-expanded='false'
+          ) 師資陣容
+          .dropdown-menu(aria-labelledby='nav2')
+            router-link.dropdown-item(to="/Staff/Fulltime") 專任師資
+            router-link.dropdown-item(to="/Staff/Parttime") 兼任師資
+            router-link.dropdown-item(to="/Staff/Director") 歷屆系主任
+            router-link.dropdown-item(to="/Staff/Administrative") 行政人員
+            router-link.dropdown-item(to="/Staff/Tutor") 各班導師
+        li.nav-item.dropdown(
+          v-for="(nav, index) in navigations",
+          :key="index",
+          :id="`nav_` + nav.navigation_id"
+        )
+          a.nav-link.active.dropdown-toggle(
+            href='#',
+            role='button',
+            data-toggle='dropdown',
+            aria-haspopup='true',
+            aria-expanded='false'
+          ) {{ nav.name }}
+          .dropdown-menu(:aria-labelledby='`nav_` + nav.navigation_id')
+            router-link.dropdown-item(
+              v-for="(subNav, subIndex) in nav.subItems",
+              :key="subIndex",
+              :to="`/content?parent=`+nav.name+`&contentKey=`+subNav.content_key"
+            ) {{ subNav.name }}
+        li.nav-item.dropdown
+          a#nav3.nav-link.active.dropdown-toggle(
+            href='#',
+            role='button',
+            data-toggle='dropdown',
+            aria-haspopup='true',
+            aria-expanded='false'
+          ) 教卓計畫
+          .dropdown-menu(aria-labelledby='nav3')
+            router-link.dropdown-item(
+              v-for="year in teachingExcellenceYears",
+              :key="year",
+              :to="`/TeachingExcellenceList/` + year"
+            ) {{ year }}學年度
+        li.nav-item.dropdown
+          a#nav4.nav-link.active.dropdown-toggle(
+            href='#',
+            role='button',
+            data-toggle='dropdown',
+            aria-haspopup='true',
+            aria-expanded='false'
+          ) 活動花絮
+          .dropdown-menu(aria-labelledby='nav4')
+            router-link.dropdown-item(to="/eventHighlignt") 活動花絮
+            router-link.dropdown-item(to="/Performance") 戲劇公演
+</template>
+<script>
+import { mapState, mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: "NavBar",
+  computed: {
+    ...mapState({
+      navigations: 'navigation'
+    }),
+    ...mapGetters(['teachingExcellenceYears'])
+  }
+}
+</script>
+<style lang="sass" scoped>
+#navbar
+  font-family: "Microsoft YaHei"
+  color: white
+  background-color: rgb(0, 87, 146)
+</style>
