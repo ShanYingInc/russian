705a6a246c588a0f76040bff5c59b2cf757ae707
tommy1994111 <tommy1994111@gmail.com>
2018-05-23 07:39:06 +0800

    修改右上角連結外觀(bootstrap4 button)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,65 +2,27 @@
 #header
   .header
     router-link.logo(to='/')
-      img(src='../assets/logo.png' ,height = '80' )
+      img(src='../assets/logo.png' ,height=80)
     .goindex
       ul
-        li.test(span='')
-          router-link(to='/') 回首頁 |
         li
-          a(href='http://www.tku.edu.tw/')  淡江大學 |
+          router-link.btn.btn-link.btn-sm(
+            to='/',
+            tabindex='-1',
+            role='button'
+          ) 回首頁
         li
-          span(@click="switchLanguage(`tw`)")  中文版 |
+          router-link.btn.btn-link.btn-sm(
+            to='http://www.tku.edu.tw/',
+            tabindex='-1',
+            role='button'
+          ) 淡江大學
         li
-          span(@click="switchLanguage(`en`)")  English |
+          span.btn.btn-link.btn-sm(@click="switchLanguage(`tw`)") 中文版
         li
-          span(@click="switchLanguage(`ru`)")  Русский
-  //- .nav
-  //-   b-nav
-  //-     b-nav-item(to="/newsList") 最新消息
-  //-     b-nav-item-dropdown#nav-2(text='系所簡介')
-  //-       b-dropdown-item(to="/Introduction/Important") 重要記事
-  //-       b-dropdown-item(to="/Introduction/Vision") 願景及策略
-  //-     b-nav-item-dropdown#nav-3(text='師資陣容')
-  //-       b-dropdown-item(to="/Staff/Fulltime") 專任師資
-  //-       b-dropdown-item(to="/Staff/Parttime") 兼任師資
-  //-       b-dropdown-item(to="/Staff/Director") 歷屆系主任
-  //-       b-dropdown-item(to="/Staff/Administrative") 行政人員
-  //-       b-dropdown-item(to="/Staff/Tutor") 各班導師
-  //-       //- b-dropdown-item(to="/Enrollment/Answer") 招生訊息
-  //-     //- b-nav-item-dropdown#nav-4(text='學期課表')
-  //-     //-   b-dropdown-item(to="/Class/Courseinformation") 課程資訊
-  //-     //-   b-dropdown-item(to="/Class/Course") 課程架構
-  //-     //-   b-dropdown-item(to="/Class/Alternative") 必選修科目
-  //-     //-   b-dropdown-item(to="/Class/Required") 替代科目
-  //-     //-   b-dropdown-item(to="/Class/Relevant") 相關修業規定
-  //-     //-   b-dropdown-item(to="/Class/Credit") 學分學程
-  //-     //-   b-dropdown-item(to="/Class/Auxiliary") 輔系雙學位
-  //-     //- b-nav-item-dropdown#nav-5(text='招生資訊')
-  //-     //-   b-dropdown-item(to="/Enrollment/Admission") 申請入學
-  //-     //-   b-dropdown-item(to="/Enrollment/Answer") 相關問題和解答
-  //-     //-   b-dropdown-item(to="/Winning") 獲獎紀錄
-  //-     //- b-nav-item-dropdown#nav-6(text='教卓計畫')
-  //-     //-   b-dropdown-item(to="/Teaching/Teaching100") 100學年度
-  //-     //-   b-dropdown-item(to="/Teaching/Teaching101") 101學年度
-  //-     //-   b-dropdown-item(to="/Teaching/Teaching102") 102學年度
-  //-     //- b-nav-item(to="/Friends/Friendsintroduction") 系友專區
-  //-     b-nav-item-dropdown(
-  //-       v-for="(nav, index) in navigations",
-  //-       :key="index",
-  //-       :text='nav.name',
-  //-       :id="`nav_` + nav.navigation_id"
-  //-     )
-  //-       b-dropdown-item(
-  //-         v-for="(subNav, subIndex) in nav.subItems",
-  //-         :key="subIndex",
-  //-         :to="`/content?parent=`+nav.name+`&contentKey=`+subNav.content_key"
-  //-       ) {{ subNav.name }}
-  //-     b-nav-item-dropdown#nav-4(text='教卓計畫')
-  //-       b-dropdown-item(v-for="year in teachingExcellenceYears", :key="year", :to="`/TeachingExcellenceList/` + year") {{ year }}學年度
-  //-     b-nav-item-dropdown#nav-4(text='活動花絮')
-  //-       b-dropdown-item(to="/eventHighlignt") 活動花絮
-  //-       b-dropdown-item(to="/Performance") 戲劇公演
+          span.btn.btn-link.btn-sm(@click="switchLanguage(`en`)") English
+        li
+          span.btn.btn-link.btn-sm(@click="switchLanguage(`ru`)") Русский
   NavBar
 </template>
 <script>
@@ -91,37 +53,20 @@ export default {
   flex-direction: column
   background: white
   .goindex
+    position: absolute
+    right: 10px
+    top: 0px
     padding: 10px 0 0 0
+    ul li
+      display: inline
+    ul li a ,span
+      font-size: 15px
+      font-family: "Microsoft YaHei"
+      color: #005792
+      font-weight: 600
+    li:not(:last-child):after
+      content: '|'
+      color: #005792
   .header
-    padding: 10px
-  // .nav
-  //   font-family: "Microsoft YaHei"
-  //   width: 100%
-  //   height: 100%
-  //   background: #005792
-  //   .dropdown-menu > li > a
-  //     color: white
-  //   .dropdown-item.active
-  //     background: white
-  //   ul
-  //     li
-  //       a
-  //         color: white
-  //       div
-  //         a
-  //           color: black
-
-.goindex ul li
-  display: inline;
-
-.goindex
-  position: absolute
-  right: 22vh
-  top: -1vh
-
-.goindex ul li a ,span
-  font-size: 15px
-  font-family: "Microsoft YaHei"
-  color: #005792
-  font-weight: 600
+    padding: 40px 20px 20px 20px
 </style>
