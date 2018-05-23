7dcd38aa474654c88f9c1ac9e803a89c2c8d9cc3
tommy1994111 <tommy1994111@gmail.com>
2018-05-24 01:17:54 +0800

    修改TopNavbar元素(簡易化)

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,24 +1,23 @@
 <template lang="pug">
 #top-navbar
-  ul
-    li
-      router-link.btn.btn-link.btn-sm(
-        to='/',
-        tabindex='-1',
-        role='button'
-      ) 回首頁
-    li
-      a.btn.btn-link.btn-sm(
-        herf='http://www.tku.edu.tw/',
-        tabindex='-1',
-        role='button'
-      ) 淡江大學
-    li
-      span.btn.btn-link.btn-sm(@click="switchLanguage(`tw`)") 中文版
-    li
-      span.btn.btn-link.btn-sm(@click="switchLanguage(`en`)") English
-    li
-      span.btn.btn-link.btn-sm(@click="switchLanguage(`ru`)") Русский
+  .item
+    router-link.btn.btn-link.btn-sm(
+      to='/',
+      tabindex='-1',
+      role='button'
+    ) 回首頁
+  .item
+    a.btn.btn-link.btn-sm(
+      herf='http://www.tku.edu.tw/',
+      tabindex='-1',
+      role='button'
+    ) 淡江大學
+  .item
+    button.btn.btn-link.btn-sm(@click="switchLanguage(`tw`)") 中文版
+  .item
+    button.btn.btn-link.btn-sm(@click="switchLanguage(`en`)") English
+  .item
+    button.btn.btn-link.btn-sm(@click="switchLanguage(`ru`)") Русский
 </template>
 <script>
 export default {
@@ -27,17 +26,16 @@ export default {
 </script>
 <style lang="sass" scoped>
 #top-navbar
-  ul
-    padding: 10px
-    margin: 0
-    li
-      display: inline
-    a ,span
+  padding: 10px
+  margin: 0
+  .item
+    display: inline
+    a ,button
       font-size: 15px
       font-family: "Microsoft YaHei"
       color: #005792
       font-weight: 600
-    li:not(:last-child):after
-      content: '|'
-      color: #005792
+  .item:not(:last-child):after
+    content: '|'
+    color: #005792
 </style>
