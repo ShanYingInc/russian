9eff3ad232be3ba204edbd7a7017fc4a61c2dc06
tommy1994111 <tommy1994111@gmail.com>
2018-05-24 00:11:37 +0800

    將右上方導覽列獨立為一列並移動到左上方

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,20 @@
 <template lang="pug">
 #header
+  TopNavbar
   .header
     router-link.logo(to='/')
       img(src='../assets/logo.png' ,height=80)
-    .goindex
-      ul
-        li
-          router-link.btn.btn-link.btn-sm(
-            to='/',
-            tabindex='-1',
-            role='button'
-          ) 回首頁
-        li
-          router-link.btn.btn-link.btn-sm(
-            to='http://www.tku.edu.tw/',
-            tabindex='-1',
-            role='button'
-          ) 淡江大學
-        li
-          span.btn.btn-link.btn-sm(@click="switchLanguage(`tw`)") 中文版
-        li
-          span.btn.btn-link.btn-sm(@click="switchLanguage(`en`)") English
-        li
-          span.btn.btn-link.btn-sm(@click="switchLanguage(`ru`)") Русский
   NavBar
 </template>
 <script>
 import NavBar from './Navbar'
+import TopNavbar from './TopNavbar'
 import { mapState, mapActions, mapGetters } from 'vuex'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    TopNavbar
   },
   computed: {
     ...mapState({
@@ -52,21 +35,6 @@ export default {
   display: flex
   flex-direction: column
   background: white
-  .goindex
-    position: absolute
-    right: 10px
-    top: 0px
-    padding: 10px 0 0 0
-    ul li
-      display: inline
-    ul li a ,span
-      font-size: 15px
-      font-family: "Microsoft YaHei"
-      color: #005792
-      font-weight: 600
-    li:not(:last-child):after
-      content: '|'
-      color: #005792
   .header
-    padding: 40px 20px 20px 20px
+    padding: 0px 20px 20px 0px
 </style>
---
+++ src/components/TopNavbar.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+#top-navbar
+  ul
+    li
+      router-link.btn.btn-link.btn-sm(
+        to='/',
+        tabindex='-1',
+        role='button'
+      ) 回首頁
+    li
+      a.btn.btn-link.btn-sm(
+        herf='http://www.tku.edu.tw/',
+        tabindex='-1',
+        role='button'
+      ) 淡江大學
+    li
+      span.btn.btn-link.btn-sm(@click="switchLanguage(`tw`)") 中文版
+    li
+      span.btn.btn-link.btn-sm(@click="switchLanguage(`en`)") English
+    li
+      span.btn.btn-link.btn-sm(@click="switchLanguage(`ru`)") Русский
+</template>
+<script>
+export default {
+  name: 'TopNavbar'
+}
+</script>
+<style lang="sass" scoped>
+#top-navbar
+  ul
+    padding: 10px
+    margin: 0
+    li
+      display: inline
+    a ,span
+      font-size: 15px
+      font-family: "Microsoft YaHei"
+      color: #005792
+      font-weight: 600
+    li:not(:last-child):after
+      content: '|'
+      color: #005792
+</style>
