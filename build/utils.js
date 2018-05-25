836d344907c5f371c2001846907fb1a97687d818
tommy1994111 <tommy1994111@gmail.com>
2018-05-25 10:47:07 +0800

    新增 sass-resource-loader

--- build/utils.js
+++ build/utils.js
@@ -59,7 +59,15 @@ exports.cssLoaders = function (options) {
     css: generateLoaders(),
     postcss: generateLoaders(),
     less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
+    sass: generateLoaders('sass', { indentedSyntax: true })
+      .concat(
+        {
+          loader: 'sass-resources-loader',
+          options: {
+            resources: path.resolve(__dirname, '../src/sass/global.sass'),
+          }
+        }
+      ),
     scss: generateLoaders('sass'),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
