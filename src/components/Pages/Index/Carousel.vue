9ad6f286279ea0ba70ebd2ecb30fa93c702ab904
tommy1994111 <tommy1994111@gmail.com>
2018-05-25 22:44:42 +0800

    Carousel 原使用bootstrapVue改用原生bootstrap

--- src/components/Pages/Index/Carousel.vue
+++ src/components/Pages/Index/Carousel.vue
@@ -1,8 +1,22 @@
 <template lang="pug">
-#carousel
-  b-carousel#banner(style='text-shadow: 1px 1px 2px #333; height: 400px; overflow: hidden;', controls='', indicators='', background='rgba(0,0,0,0.3)', :interval='4000', v-model='slide', @sliding-start='onSlideStart', @sliding-end='onSlideEnd')
-    b-carousel-slide(v-for = '(image) in images', :key='image.path')
-      img.d-flex(:src="$api.rootLink + image.path" slot='img', height='400')
+#carousel.carousel.slide(data-ride='carousel')
+  //- b-carousel#banner(style='text-shadow: 1px 1px 2px #333; height: 400px; overflow: hidden;', controls='', indicators='', background='rgba(0,0,0,0.3)', :interval='4000', v-model='slide', @sliding-start='onSlideStart', @sliding-end='onSlideEnd')
+  //-   b-carousel-slide(v-for = '(image) in images', :key='image.path')
+  //-     img.d-flex(:src="$api.rootLink + image.path" slot='img', height='400')
+  ol.carousel-indicators()
+    li(data-target='#carousel', v-for='(image, index) in images', :key='image.path', :data-slide-to='index')
+  .carousel-inner
+    .carousel-item(v-for='(image, index) in images', :key='image.path', :class="{ active: index == 0 }")
+      img.d-block.w-100(:src="$api.rootLink + image.path", alt='First slide')
+      .carousel-caption.d-none.d-md-block(style="background: rgba(0, 0, 0, .3)")
+        h5 測試
+        p 測試
+  a.carousel-control-prev(href='#carousel', role='button', data-slide='prev')
+    span.carousel-control-prev-icon(aria-hidden='true')
+    span.sr-only Previous
+  a.carousel-control-next(href='#carousel', role='button', data-slide='next')
+    span.carousel-control-next-icon(aria-hidden='true')
+    span.sr-only Next
 </template>
 <script>
 import { mapState } from 'vuex'
