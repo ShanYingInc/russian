8bddc70a49a9f808a6fe2f5177b0f051a7dd77cc
tommy1994111 <tommy1994111@gmail.com>
2018-05-27 17:45:32 +0800

    加入全域css

--- src/main.js
+++ src/main.js
@@ -4,14 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import BootstrapVue from 'bootstrap-vue'
+import i18n from './i18n'
 import ApiPlugin from './api/ApiPlugin'
-
-import './assets/bootstrap-edit.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './main.css'
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
 Vue.use(ApiPlugin)
 
 /* eslint-disable no-new */
@@ -19,6 +16,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   template: '<App/>',
   components: { App }
 })
