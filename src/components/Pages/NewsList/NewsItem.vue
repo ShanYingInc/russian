ab5ff68710845e51849d545ba03e41b04bcfa389
tommy1994111 <tommy1994111@gmail.com>
2018-05-28 17:38:56 +0800

    新增NewsItem(未完成)

---
+++ src/components/Pages/NewsList/NewsItem.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+router-link#newsItem(:to="`/news/${NewsItem.news_id}`")
+  .type(:style="{ background: newsCategoryColor }") #[span {{ $t(newsCategoryName) }}]
+  .title {{ NewsItem.title }}
+  .date {{ new Date(NewsItem.created_on).toLocaleDateString() }}
+</template>
+<script>
+const newsCategory = {
+  school: {
+    name: "newsCategory.school",
+    color: "#f44336",
+    hover: "#ff7961"
+  },
+  speech: {
+    name: "newsCategory.speech",
+    color: "#9c27b0",
+    hover: "#d05ce3"
+  },
+  enrollment: {
+    name: "newsCategory.enrollment",
+    color: "#2196f3",
+    hover: "#6ec6ff"
+
+  },
+  recruitment: {
+    name: "newsCategory.recruitment",
+    color: "#4caf50",
+    hover: "#80e27e"
+  },
+  scholarship: {
+    name: "newsCategory.scholarship",
+    color: "#ffeb3b",
+    hover: "#ffff72"
+  },
+  others: {
+    name: "newsCategory.others",
+    color: "#ff9800",
+    hover: "#ffc947"
+  }
+}
+
+export default {
+  name: 'NewsItem',
+  props: {
+    newsData: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    newsCategoryName () {
+      return newsCategory[this.newsData.type].name
+    },
+    newsCategoryColor () {
+      return newsCategory[this.newsData.type].color
+    }
+  }
+}
+</script>
+<style lang="sass" scoped>
+#newsItem
+</style>
